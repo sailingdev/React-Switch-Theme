53d5d3289328bbbcf1f280b052ef0bbca9ee2191
sailingdev <sailingdev123@gmail.com>
2020-04-19 12:34:13 +1000

    Add Card Component

---
+++ src/components/Card/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <div className="Card">
+      <div className="img" />
+      <h2>{props.title || "Hello World"} </h2>
+      <p>
+        {props.description ||
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do siusmod tempor incididunt ut labore et dolore magna aliqua."}
+      </p>
+      <button>Read More </button>
+    </div>
+  );
+};
+
+export default Card;
